958106f1e447331249ea93ff028b85c75c4d8853
Zeyuan Li <zyli5313@gmail.com>
2013-10-19 17:32:39 -0400

    Finish the DP solution on ctax.paper

---
+++ single.r
@@ -0,0 +1,115 @@
+# single risky asset, replicate ctax paper
+
+#--- constant ---
+# NDIM = 500
+# NTIMEUP = 100
+# NTIMELOW = 20
+
+NDIM = 3
+NTIMEUP = 4
+NTIMELOW = 1
+STEP = 10
+
+#--- parameters ---
+
+i = 0.035 #annual inflation rate
+r = 0.06 # nominal pretax interest rate on riskless bond
+d = 0.02 # nominal dividend yield
+tau_d = 0.36 # tax rate on dividend and interest
+tau_g = 0.36 # tax rate on capital gain and losses
+beta = 0.96 # annual subjective discount factor
+gamma = 3.0 # risk aversion parameter
+H = NTIMEUP # num of period of annuity for the benefit of investor's beneficiary 
+
+rstar = ((1-tau_d) * r - i) / (1 + i) # after tax real bond return 
+A_H = (rstar * (1+rstar)^H) / ((1+rstar)^H - 1) 
+
+# states xt
+s = seq(0, 1.0, by=NDIM)
+pstar = seq(0, 1.0, by=NDIM)
+
+# get mortality rate
+lambda = get_mortality_rate_vec()
+
+# main
+# value function (0-NTIMELOW not used)
+v = array(0, dim=c(NDIM, NDIM, NTIMEUP))
+# initialization
+v[ , , NTIMEUP] = (beta * (1-beta^H) * A_H^(1-gamma)) / ((1-beta) * (1-gamma))
+# vector to save optimal comsuption ratio
+c = array(0, dim=c(NDIM, NDIM, NTIMEUP))
+# vector to save equity ration in total wealth after time t
+f = array(0, dim=c(NDIM, NDIM, NTIMEUP))
+# vector to save bond ration in total wealth after time t
+b = array(0, dim=c(NDIM, NDIM, NTIMEUP))
+
+# TODO: not yet included eq 13
+# v[st][pstar_tm1][t]
+for(t in NTIMEUP-1:NTIMELOW) {
+	lambdat = lambda[t]
+
+	# (scalar) E_t[v_{t+1}(x_{t+1})]
+	Et_vtp1 = mean(v[ , , t+1]);
+
+	for(is in s) {
+		for(ipstar in pstar) {
+			# current states: st, pstar_tm1 (scalar)
+			st = s[is]
+			pstar_tm1 = pstar[ipstar]
+
+			# current policies: ft, bt, ct (vector)
+			curbase = seq(0, 1, by=1/STEP)
+			# generate each posible (ft[i], bt[i]) pair. Can calculate ct[i] using eq 14
+			ft = rep(curbase, each=STEP)
+			bt = rep(curbase, times=STEP)
+			valid_idx = ft + bt <= 1
+			ft = ft[valid_idx]
+			bt = bt[valid_idx]
+			
+			# part 1
+			# calc ct
+			# (vector) fraction of beginning-of-period wealth that is taxable as realized capital gain in period t
+			deltat = ((pstar_tm1 > 1) * st + (pstar_tm1 <= 1) * max(sf-ft, 0)) * (1 - pstar_tm1)
+			ct = 1 - tau_g * deltat - ft - bt
+			
+			res1 = exp(-lambdat) * ct^(1-gamma) / (1-gamma)
+
+			# part 2
+			res2 = (1-exp(-lambdat) * beta * (1-beta^H) * A_H^(1-gamma)) / ((1-beta) * (1-gamma))
+
+			# part 3
+			# TODO: need to represent g_tp1
+			g_tp1 = 1 / pstar_tm1 - 1
+			# (vector) gross nomial return from t to t+1 (paid dividend tax, but not paid capital gain tax)
+			R_tp1 = (ft * (1+(1-tau_d)*d) * (1+g_tp1) + (1+(1-tau_d)*r) * bt) / (ft + bt)
+			# vector 100x1
+			w_tp1 = R_tp1 * (1 - tau_g * deltat - ct)
+
+			# w_{t+1} is independent of E_t[v_{t+1}(x_{t+1})] 
+			res3 = exp(-lambdat) * beta * Et_vtp1 * w_tp1^(1-gamma)
+
+			# update value function v
+			vt = res1 + res2 + res3
+			v[is][ipstar][t] = max(vt)
+			# find the optimal policy
+			idx = which.max(vt)
+			f[is][ipstar][t] = ft[idx]
+			c[is][ipstar][t] = ct[idx]
+			b[is][ipstar][t] = bt[idx]
+		}
+	}
+	
+}
+
+# return mortality rate in age = [1, 100], filter 0,101,102
+get_mortality_rate_vec = function() {
+	life_len_raw = read.table("mortality_rate_1980.txt", header=TRUE)
+	life_len = c(0.5 * (life_len_raw[ , 2] + life_len_raw[ , 3]), 
+							0.5 * (life_len_raw[ , 5] + life_len_raw[ , 6]), 
+							0.5 * (life_len_raw[ , 8] + life_len_raw[ , 9]) )
+
+	morta_rate = 1 - life_len[-c(length(life_len))] / (life_len[-c(1)] + 1)
+	morta_rate = morta_rate[1:100]
+
+	return(morta_rate)
+}
